85748442727dd73f26d484ec02c4626c8e18d76d
Daniel López <dankzer1@gmail.com>
2015-05-07 23:14:57 -0500

    Create app.java

---
+++ Students/app.java
@@ -0,0 +1,14 @@
+public class app {
+    public static void main(String[] args) {
+ 
+        Student student1 = new Student("Daniel López","1CM11",4,new Date(2,6,1995)); 
+ 
+        student1.assignSubject(new Subject("Física",9,1));
+        student1.assignSubject(new Subject("Cálculo",7,1));
+        student1.assignSubject(new Subject("POO",10,2));
+        student1.assignSubject(new Subject("Microcontroladores",8,4));
+ 
+        System.out.println(student1);
+        student1.listSubjects();
+    }
+}
